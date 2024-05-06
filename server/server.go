3f38f4959a6daedea7378c87a705d269992bcc36
Piggey <pepcio3320@gmail.com>
2024-05-06 19:27:50 +0200

    testing listening on port

--- server/server.go
+++ server/server.go
@@ -1,23 +1,25 @@
 package server
 
 import (
+	"fmt"
 	"log/slog"
 	"net"
-	"net/netip"
 	"os"
 
 	"github.com/Piggey/bsr/util"
 )
 
 type Server struct {
-	udpAddr *net.UDPAddr
+	addr    net.Addr
 	udpConn *net.UDPConn
 	logger  *slog.Logger
 }
 
 func NewServer(addr string) *Server {
-	addrPort := netip.MustParseAddrPort(addr)
-	udpAddr := net.UDPAddrFromAddrPort(addrPort)
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		panic(err)
+	}
 
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
@@ -30,10 +32,10 @@ func NewServer(addr string) *Server {
 	})
 	logger := slog.New(serverHandler)
 
-	logger.Info("ok dziala?", slog.Bool("kutas", true))
+	logger.Info("server created")
 
 	return &Server{
-		udpAddr: udpAddr,
+		addr:    udpAddr,
 		udpConn: udpConn,
 		logger:  logger,
 	}
@@ -43,6 +45,22 @@ func (s *Server) Close() error {
 	return s.udpConn.Close()
 }
 
-func (s *Server) Listen() {
+func (s *Server) Listen() error {
+	s.logger.Info("started listening")
+
+	for {
+		// wait for client to create a new game
+		_, addr, err := s.udpConn.ReadFrom(buf)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("received packet from addr: %v\n", addr)
+
+		// create new go routine for a game
+	}
+}
+
+func (s *Server) StartNewGame() {
 	panic("to be implemented")
 }
