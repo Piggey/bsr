e76d9082bd8dfb27b14d1cdfcd1a1b04205419f1
Piggey <pepcio3320@gmail.com>
2024-05-06 19:27:26 +0200

    create client code

--- client/client.go
+++ client/client.go
@@ -1,3 +1,53 @@
 package client
 
-type Client struct{}
+import (
+	"encoding/binary"
+	"log/slog"
+	"net"
+	"os"
+
+	"github.com/Piggey/bsr/packet"
+	"github.com/Piggey/bsr/util"
+)
+
+type Client struct {
+	addr    net.Addr
+	udpConn *net.UDPConn
+	logger  *slog.Logger
+}
+
+func NewClient(serverAddr string) *Client {
+	udpAddr, err := net.ResolveUDPAddr("udp", serverAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	udpConn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	clientHandler := util.NewCustomHandler("client", "", os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	})
+	logger := slog.New(clientHandler)
+
+	logger.Info("client connected")
+
+	return &Client{
+		addr:    udpConn.LocalAddr(),
+		udpConn: udpConn,
+		logger:  logger,
+	}
+}
+
+func (c *Client) Close() error {
+	return c.udpConn.Close()
+}
+
+func (c *Client) CreateNewGame(mode packet.GameMode) error {
+	ngp := packet.NewCreateGamePacket(mode)
+
+	return binary.Write(c.udpConn, binary.BigEndian, ngp)
+}
--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/Piggey/bsr/client"
 	"github.com/Piggey/bsr/server"
 )
 
 func main() {
 	fmt.Println("siema witam")
-	srv := server.NewServer("127.0.0.1:5000")
+	srv := server.NewServer(":5000")
+	defer srv.Close()
+
+	srv.Listen()
 	fmt.Printf("srv: %v\n", srv)
+
+	client := client.NewClient("127.0.0.1:5000")
+	defer client.Close()
+
+	fmt.Printf("client: %v\n", client)
 }
