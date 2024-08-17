cdcc11f68075a20d17f3c0b1991e54c0c8195fdd
Piggey <pepcio3320@gmail.com>
2024-08-17 12:51:58 +0200

    move args to main.go

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Piggey/bsr/client"
 	"github.com/Piggey/bsr/packet"
 	"github.com/Piggey/bsr/server"
 	"github.com/alecthomas/kong"
 )
 
+var args struct {
+	Client struct {
+		ServerAddr string  `help:"server address"`
+		Addr       *string `help:"optional client address, defaults to first available port"`
+		Mode       string  `help:"against other player (pvp) or against ai (pve)" default:"pve"`
+		// Pvp        struct {
+		// 	OpponentAddr string `help:"opponents address"`
+		// } `default:"1" help:"play against other client"`
+	} `cmd:"" help:"run as client"`
+	Server struct {
+		Addr string `help:"server address" default:":5000"`
+	} `cmd:"" help:"run as server"`
+}
+
 func main() {
-	ctx := kong.Parse(&Args)
+	ctx := kong.Parse(&args)
+
+	fmt.Printf("args: %v\n", args)
 
 	// open server
 	// connect client
@@ -21,13 +39,16 @@ func main() {
 
 	switch ctx.Command() {
 	case "server":
-		srv := server.NewServer(Args.Server.Addr)
+		srv := server.NewServer(args.Server.Addr)
 		defer srv.Close()
 
-		srv.Listen()
+		err := srv.Listen()
+		if err != nil {
+			panic(err)
+		}
 
 	case "client":
-		client := client.NewClient(Args.Client.ServerAddr)
+		client := client.NewClient(args.Client.ServerAddr)
 		defer client.Close()
 
 		err := client.StartNewGame(packet.GameModePvE) // for now
